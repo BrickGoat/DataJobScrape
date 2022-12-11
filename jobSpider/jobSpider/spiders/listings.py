42007dbbdb2f8fbd9a992e818bf1164e91d3f262
BrickGoat <BrickGoat@gmail.com>
2022-12-11 15:05:25 -0500

    switched to dict of selectors and fixed other minor errors

--- jobSpider/jobSpider/spiders/listings.py
+++ jobSpider/jobSpider/spiders/listings.py
@@ -1,5 +1,6 @@
 import scrapy
 import time
+from scrapy.shell import inspect_response
 from selenium.webdriver.common.action_chains import ActionChains
 from scrapy_selenium import SeleniumRequest
 from selenium.webdriver.common.by import By
@@ -18,11 +19,33 @@ import re
         }
 """
 url_max_pages = {
-    "usajobs": [0,15],
+    "usajobs": [0, 15],
     "progressivedatajobs": [0, 15],
     "outerjoin": [0, 15],
     "weworkremotely": None,
     "weworkremotely": None,
+}
+
+url_selectors = {
+    "usajobs": {
+        "listings": (
+            By.XPATH,
+            "//div[@id='usajobs-search-results']/div[@class='usajobs-search-result--core']/a",
+        ),
+        "next": (By.XPATH, "//a[@title='Go To Next Page']"),
+    },
+    "progressivedatajobs": {
+        "listings": (By.XPATH, "//div[@class='grid-job']/h2/a"),
+        "next": ("rel_link", "/job-postings/?wpv_view_count=627&wpv_paged=1"),
+    },
+    "outerjoin": {
+        "listings": (By.XPATH, "//div[@class='w-full ml-4']/a"),
+        "next": (By.XPATH, "(//nav/a[@rel='next'])"),
+    },
+    "weworkremotely": {
+        "listings": (By.XPATH, "(//article/ul/li[@class='feature']/a)"),
+        "next": (None, None),
+    },
 }
 
 
@@ -32,14 +55,14 @@ class ListingSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             "https://www.usajobs.gov/Search/Results?j=2210&j=1550&j=1560&k=data&p=1",
-            "https://www.progressivedatajobs.org/job-postings",
-            "https://outerjoin.us/?q=data",
-            "https://weworkremotely.com/categories/remote-back-end-programming-jobs",
-            "https://weworkremotely.com/categories/remote-full-stack-programming-jobs",
+            # "https://www.progressivedatajobs.org/job-postings",
+            # "https://outerjoin.us/?q=data",
+            # "https://weworkremotely.com/categories/remote-back-end-programming-jobs",
+            # "https://weworkremotely.com/categories/remote-full-stack-programming-jobs",
         ]
         # Selectors for getting next page
         next = [
-            (By.XPATH, "//a[@title='Go To Next Page']/@aria-hidden"),
+            (By.XPATH, "//a[@title='Go To Next Page']"),
             ("rel_link", "/job-postings/?wpv_view_count=627&wpv_paged=1"),
             (By.XPATH, "(//nav/a[@rel='next'])"),
             ("na", "na"),
@@ -61,7 +84,7 @@ class ListingSpider(scrapy.Spider):
                 urls[i],
                 10,
                 self.parsePage,
-                next[i],
+                # next[i],
                 listing_divs[i],
             )
             for i in range(len(urls))
@@ -70,10 +93,16 @@ class ListingSpider(scrapy.Spider):
 
     # , next_selector, listing_selector
     def parsePage(self, response):
-        # from scrapy.shell import inspect_response
         # inspect_response(response, self)
-        next_selector = response.request.meta.get("next_selector")
-        listing_selector = response.request.meta.get("listing_selector")
+        driver = response.request.meta["driver"]
+        url = driver.current_url
+        key = ""
+        for key_ in url_max_pages.keys():
+            if re.search(key_, url) is not None:
+                key = key_
+
+        next_selector = url_selectors[key]["next"]
+        listing_selector = url_selectors[key]["listings"]
         containers = []
         if listing_selector[0] == By.CLASS_NAME:
             containers = response.xpath(f"//div[@class='{listing_selector[1]}']")
@@ -86,12 +115,9 @@ class ListingSpider(scrapy.Spider):
                 yield ({"url": response.url[:end] + rel_link})
             else:
                 yield ({"url": rel_link})
-        driver = response.request.meta["driver"]
-        if next_selector[0] == "rel_link":
-            print(next_selector)
-            from scrapy.shell import inspect_response
 
-            inspect_response(response, self)
+        if next_selector[0] == "rel_link":
+            #inspect_response(response, self)
             url = (
                 driver.current_url
                 + next_selector[1][:-1]
@@ -99,36 +125,37 @@ class ListingSpider(scrapy.Spider):
             )
             yield (
                 self.makeRequest(
-                    url, 5, self.parsePage, next_selector, listing_selector
+                    url,
+                    5,
+                    self.parsePage,
+                    listing_selector,
+                    wait_until=EC.presence_of_element_located(listing_selector),
                 )
             )
             return
         # Stop scraping after n pages
-        url = driver.current_url
-        for key in url_max_pages.keys():
-            if re.search(key, url) is not None:
-                if url_max_pages[key] is None:
-                    return
-                url_max_pages[key][0]+=1
-                if url_max_pages[key][0] >= url_max_pages[key][1]:
-                    return
-        if next_selector[0] != "na":
+        if url_max_pages[key] is None:
+            return
+        url_max_pages[key][0] += 1
+        if url_max_pages[key][0] >= url_max_pages[key][1]:
+            return
+        if next_selector[0] == None:
             return
         try:
             next = response.xpath(next_selector[1])
         except Exception:
-            from scrapy.shell import inspect_response
-
             inspect_response(response, self)
-
+        #inspect_response(response, self)
         if (
             len(next) == 0
-            or next.xpath("@aria-hidden").get() == "false"
+            or next.xpath("@aria-hidden").get() == "true"
             or next.xpath("@display").get() == "none"
         ):
-            # raise Exception("NEXT IS NONE!!!!")
+            #inspect_response(response, self)
+            raise Exception(f"{next}\nNEXT IS NONE!!!!")
             return
-        while True:
+        i = 0
+        while i < 5:
             try:
                 button = WebDriverWait(driver, 10).until(
                     EC.element_to_be_clickable(next_selector)
@@ -145,27 +172,26 @@ class ListingSpider(scrapy.Spider):
                         url,
                         5,
                         self.parsePage,
-                        next_selector,
                         listing_selector,
-                        wait_until=EC.presence_of_element_located(listing_selector),
                     )
                 )
             except Exception as e:
                 print(f"EXCEPTION: {e}")
                 # time.sleep(100)
                 ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+                i+=1
                 continue
             break
 
-    def makeRequest(self, url, tm, cb, next_selector, listing_selector):
+    def makeRequest(self, url, tm, cb, listing_selector):
         request = SeleniumRequest(
             url=url,
             wait_time=tm,
             callback=cb,
             wait_until=EC.presence_of_element_located(listing_selector),
         )
-        request.meta["next_selector"] = next_selector
-        request.meta["listing_selector"] = listing_selector
-        #request.cb_kwargs["next_selector"] = next_selector
-        #request.cb_kwargs["listing_selector"] = listing_selector
+        # request.meta["next_selector"] = next_selector
+        # request.meta["listing_selector"] = listing_selector
+        # request.cb_kwargs["next_selector"] = next_selector
+        # request.cb_kwargs["listing_selector"] = listing_selector
         return request
